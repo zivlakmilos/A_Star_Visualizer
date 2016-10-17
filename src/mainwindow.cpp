213cfae440f63ffa52cb35022259a014a64fd14d
Milos Zivlak <zivlakmilos@gmail.com>
2016-10-18 00:39:47 +0200

    Do some comments

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,12 +7,15 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     /*
-     * Setting application parameters
+     * Setting core application parameters
      */
     QCoreApplication::setApplicationName(tr("A Star Visualizer"));
     QCoreApplication::setOrganizationName(tr("Milos Zivlak - ZI"));
     QCoreApplication::setOrganizationDomain(tr("http://zivlakmilos.ddns.net"));
     
+    /*
+     * Setting window title
+     */
     setWindowTitle(QString("%1    %2 %3 2016").arg(QCoreApplication::applicationName(),
                                              QChar(0xA9),   // Copyright simbol (ASCII)
                                              QCoreApplication::organizationName()));

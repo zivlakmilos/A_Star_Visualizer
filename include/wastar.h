7b98eef647370dba01f37b1ddb047e914f87ef1d
Milos Zivlak <zivlakmilos@gmail.com>
2016-10-20 13:26:43 +0200

    Added WAStar widget (for central widget)

---
+++ include/wastar.h
@@ -0,0 +1,15 @@
+#ifndef _W_STAR_H_
+#define _W_STAR_H_
+
+#include <QWidget>
+
+class WAStar : QWidget
+{
+public:
+    WAStar(QWidget *parent = 0);
+    virtual ~WAStar(void);
+    
+private:
+};
+
+#endif // _W_STAR_H_
---
+++ src/wastar.cpp
@@ -0,0 +1,10 @@
+#include "wastar.h"
+
+WAStar::WAStar(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+WAStar::~WAStar(void)
+{
+}
